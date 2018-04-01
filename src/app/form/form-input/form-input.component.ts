3126b813cd05a83a27d43b46d0fdd2c479477291
pokemzok <kozemkop@gmail.com>
2018-04-01 19:23:10 +0200

    login form

---
+++ src/app/form/form-input/form-input.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-form-input',
+  templateUrl: './form-input.component.html',
+  styleUrls: ['./form-input.component.css']
+})
+export class FormInputComponent implements OnInit {
+
+  @Input() label: string;
+  @Input() inputPlaceholder: string;
+  @Input() description: string;
+  @Input() type: string;
+  id: string = Math.random().toString(36).substring(2); /*FIXME uuid */
+
+  constructor() {
+    this.type = 'text';
+  }
+
+  ngOnInit() {
+
+  }
+
+}

0f6d42d6f806e4befdb043265d49275675b6174c
pokemzok <kozemkop@gmail.com>
2018-04-01 19:22:55 +0200

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { FormInputComponent } from './form/form-input/form-input.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FormInputComponent
   ],
   imports: [
     BrowserModule,

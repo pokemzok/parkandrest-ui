1df2a22ada7c114a02ac103ea9436faf934cf95d
pokemzok <kozemkop@gmail.com>
2018-04-02 11:54:43 +0200

    angular version up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { FormInputComponent } from './form/form-input/form-input.component';
+import {LoginComponent} from './login/login.component';
+import {FormInputComponent} from './form/form-input/form-input.component';
+import {FormSubmitComponent} from './form/form-submit/form-submit.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+/*import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}*/
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent,
     LoginComponent,
-    FormInputComponent
+    FormInputComponent,
+    FormSubmitComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TranslateModule.forRoot()
   ],
-  providers: [],
+  providers: [ ], /*FIXME internatinalization */
   bootstrap: [AppComponent]
 })
 export class AppModule { }

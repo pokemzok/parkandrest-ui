93536a3c874c037abd01c982d2f74acbb9360729
pokemzok <kozemkop@gmail.com>
2018-04-02 11:55:03 +0200

    login form

---
+++ src/app/form/form-submit/form-submit.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-form-submit',
+  templateUrl: './form-submit.component.html',
+  styleUrls: ['./form-submit.component.css']
+})
+export class FormSubmitComponent implements OnInit {
+
+  @Input() description: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

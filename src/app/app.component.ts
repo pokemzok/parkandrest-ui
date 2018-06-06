ca884f8a4650186a06c9f1eac63ddfd5533b8145
kozemkop <pavlo.kozemko@altkom.pl>
2018-06-06 08:32:10 +0200

    Issue #20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {ActivatedRoute, Event, NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +11,44 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
 
-    constructor (private translate: TranslateService) {
-      translate.setDefaultLang('en');
-      translate.use('en');
-    }
+  private _shouldBlurComponent = false;
 
+  constructor(private translate: TranslateService, private router: Router, private activatedRoute: ActivatedRoute) {
+    this.initTranslateParams(translate);
+    this.onNavigation(router, activatedRoute);
+  }
+
+  private initTranslateParams(translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
+  private onNavigation(router: Router, activatedRoute: ActivatedRoute) {
+    router.events.subscribe(
+      (event: Event) => {
+
+        if (event instanceof NavigationStart) {
+          // TODO: show loading animation
+        }
+
+        if (event instanceof NavigationEnd) {
+          // TODO: hide loading animation
+          // TODO: detect route and blur background with flag
+          this.onLoginRoute(activatedRoute);
+        }
+
+      });
+  }
+
+  private onLoginRoute(activatedRoute: ActivatedRoute) {
+    const loginRoutes = _.find(activatedRoute.children, (activatedChildRoute: any) => {
+        return activatedChildRoute.component.name === LoginComponent.name;
+      }
+    );
+    this._shouldBlurComponent = !_.isEmpty(loginRoutes);
+  }
+
+  get shouldBlurComponent(): boolean {
+    return this._shouldBlurComponent;
+  }
 }

e529d5861f8861a6104a807627630657cc6410e6
kozemkop <pavlo.kozemko@altkom.pl>
2018-07-03 13:51:31 +0200

    Issue #14

--- src/app/users/manage/users-management.component.ts
+++ src/app/users/manage/users-management.component.ts
@@ -3,6 +3,10 @@ import {UsersProviderInterface} from './users.provider.interface';
 import {UserResponse} from '../user.response';
 import {UsersFilter} from './users.filter';
 import {UserManagement} from '../new-user.interface';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {TranslatedOptionFactory} from '../../form/select/options/translated-option.factory';
+import {SelectOption} from '../../form/select/options/select-option';
+import {LabelPosition} from '../../form/LabelPosition';
 
 @Component({
   selector: 'app-users-management',
@@ -11,15 +15,35 @@ import {UserManagement} from '../new-user.interface';
 })
 export class UsersManagementComponent implements OnInit {
 
+  usersForm: FormGroup;
   users: UserResponse[];
   filter = new UsersFilter();
+  authoritiesOptions: SelectOption[];
+  booleanOptions: SelectOption[];
+  labelPosition = LabelPosition.NONE;
 
   constructor(@Inject('UsersService') private usersService: UsersProviderInterface,
-              @Inject('UserManagementService')  private userManagementService: UserManagement) {
+              @Inject('UserManagementService')  private userManagementService: UserManagement,
+              private optionsFactory: TranslatedOptionFactory
+  ) {
     this.users = usersService.get(this.filter);
+    /*
+    authoritiesOptions: SelectOption[];
+  booleanOptions: SelectOption[]; FIXME provide data
+     */
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.usersForm = new FormGroup({
+      'username': new FormControl(null, [Validators.maxLength(255)]),
+      'authority': new FormControl(null),
+      'active': new FormControl(null)
+    })
+  }
+
+  onSearch() {
+
+  }
 
   deactivate(user: UserResponse) {
     this.userManagementService.deactivate(user.username);

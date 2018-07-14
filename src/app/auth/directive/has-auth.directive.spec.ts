e7e0c05b825112aecdb3985dba32e4f973a65e72
pokemzok <kozemkop@gmail.com>
2018-07-14 12:29:55 +0200

    Issue #30

--- src/app/auth/directive/has-auth.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HasAuthDirective } from './has-auth.directive';
-
-describe('HasAuthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HasAuthDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/common/toaster/translated-toaster.service.ts
+++ src/app/common/toaster/translated-toaster.service.ts
@@ -4,32 +4,34 @@ import {ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class TranslatedToastrFacade {
- // TODO:  extract translateService operation on supply it with toasterService.command as Supplier
-  // for example pass toasterService.success as a parameter
+
   constructor(private translateService: TranslateService, private toasterService: ToastrService) {}
 
   success(key: string | Array<string>, interpolateParams?: Object) {
-    this.translateService.get(key, interpolateParams).subscribe((notification: string) => {
-      this.toasterService.success(notification);
-    });
+    this.translate(key, interpolateParams, (notification: string) =>
+      this.toasterService.success(notification)
+    );
   }
 
   error(key: string | Array<string>, interpolateParams?: Object) {
-    this.translateService.get(key, interpolateParams).subscribe((notification: string) => {
-      this.toasterService.error(notification);
-    });
+    this.translate(key, interpolateParams, (notification: string) =>
+      this.toasterService.error(notification)
+    );
   }
 
   info(key: string | Array<string>, interpolateParams?: Object) {
-    this.translateService.get(key, interpolateParams).subscribe((notification: string) => {
-      this.toasterService.info(notification);
-    });
+    this.translate(key, interpolateParams, (notification: string) =>
+      this.toasterService.info(notification)
+    );
   }
 
   warning(key: string | Array<string>, interpolateParams?: Object) {
-    this.translateService.get(key, interpolateParams).subscribe((notification: string) => {
+    this.translate(key, interpolateParams, (notification: string) =>
       this.toasterService.warning(notification)
-    });
+    );
   }
 
+  private translate(key: string | Array<string>, interpolateParams?: Object, subscribeAction?: (value: string) => void) {
+    this.translateService.get(key, interpolateParams).subscribe(subscribeAction);
+  }
 }

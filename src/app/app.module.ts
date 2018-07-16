14c4ecff5ad758682034a9e02304ee6e3bffbfda
pokemzok <kozemkop@gmail.com>
2018-07-16 19:26:35 +0200

    modularization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import {LoginComponent} from './authentication/login/login.component';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {FooterComponent} from './footer/footer.component';
@@ -10,14 +11,32 @@ import {CookieService} from 'ngx-cookie-service';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {AuthCookiesService} from './security/cookies/authcookies.service';
 import {RouterModule} from '@angular/router';
+import {UsersComponent} from './users/users.component';
+import {AccountMonitoringComponent} from './accountmonitoring/accountmonitoring.component';
 import {ToastrModule} from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {TranslatedToastrFacade} from './common/toaster/translated-toaster.service';
 import {HeaderComponent} from './header/header.component';
 import {ModalModule} from 'ngx-modal';
-import {DateAdapter} from '@angular/material';
+import {DateAdapter, MatTabsModule} from '@angular/material';
+import {MomentModule} from 'ngx-moment';
+import {MockFinancialReportService} from './accountmonitoring/report/mockfinancialreport.service';
+import {Provider} from '@angular/core/src/di/provider';
+import {MockAuthService} from './security/mockauth.service';
+import {ENVIRONMENT} from '../environments/environment';
 import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import {AuthenticationService} from './security/authentication.service';
+import {FinancialReportService} from './accountmonitoring/report/financialreport.service';
+import {MockParkingSpaceService} from './parkingmeter/mock.parkingspace.service';
+import {ParkingSpaceService} from './parkingmeter/parkingspace.service';
 import {TranslatedOptionFactory} from './form/select/options/translated-option.factory';
+import {UsersManagementComponent} from './users/manage/users-management.component';
+import {NewUserComponent} from './users/new/new-user.component';
+import {MockUserManagementService} from './users/mock.user-management.service';
+import {UserManagementService} from './users/user-management.service';
+import {MockUsersService} from './users/manage/mock.users.service';
+import {UsersService} from './users/manage/users.service';
+import {LogoutComponent} from './authentication/logout/logout.component';
 import {LoginAuthGuard} from './security/guard/login-authguard.service';
 import {ROUTES_DEFINITIONS} from './routes-definitions';
 import {OwnerAuthGuard} from './security/guard/owner-authguard.service';
@@ -29,18 +48,13 @@ import {HasAuthDirective} from './security/directive/has-auth.directive';
 import {StoreModule} from '@ngrx/store';
 import {authorityReducer} from './security/store/authority-reducer';
 import {StoreInitializer} from './security/store/store-initializer';
+import {UsernameValidator} from './users/new/validator/username.validator.interface';
+import {MockUsernameValidator} from './users/new/validator/mock.username.validator';
+import {AsyncUsernameValidator} from './users/new/validator/async.username.validator';
 import {DrivermockModule} from './drivermock/drivermock.module';
 import {CommonsModule} from './common/commons.module';
-import {ParkingMeterModule} from './parkingmeter/parkingmeter.module';
-import {AccountMonitoringModule} from './accountmonitoring/accountmonitoring.module';
-import {UsersModule} from './users/users.module';
-import {LoginComponent} from './authentication/login/login.component';
-import {LogoutComponent} from './authentication/logout/logout.component';
-import {AuthenticationService} from './security/authentication.service';
-import {ENVIRONMENT} from '../environments/environment';
-import {MockAuthService} from './security/mockauth.service';
-import {Provider} from '@angular/core/src/di/provider';
 import {FormModule} from './form/form.module';
+import {ParkingMeterComponent} from './parkingmeter/parkingmeter.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -57,12 +71,22 @@ function provideServices(): any[] {
 function provideMockServices(): Provider[] {
   return [
     {provide: 'AuthService', useClass: MockAuthService},
+    {provide: 'FinancialReportService', useClass: MockFinancialReportService},
+    {provide: 'ParkingSpaceService', useClass: MockParkingSpaceService},
+    {provide: 'UserManagementService', useClass: MockUserManagementService},
+    {provide: 'UsersService', useClass: MockUsersService},
+    {provide: 'UsernameValidator', useClass: MockUsernameValidator},
   ]
 }
 
 function provideBackendServices(): Provider[] {
   return [
     {provide: 'AuthService', useClass: AuthenticationService},
+    {provide: 'FinancialReportService', useClass: FinancialReportService},
+    {provide: 'ParkingSpaceService', useClass: ParkingSpaceService},
+    {provide: 'UserManagementService', useClass: UserManagementService},
+    {provide: 'UsersService', useClass: UsersService},
+    {provide: 'UsernameValidator', useClass: AsyncUsernameValidator},
   ]
 }
 
@@ -70,11 +94,16 @@ function provideBackendServices(): Provider[] {
   declarations: [
     NavbarComponent,
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    HasAuthDirective,
     LoginComponent,
-    LogoutComponent
+    FooterComponent,
+    UsersComponent,
+    AccountMonitoringComponent,
+    ParkingMeterComponent,
+    HeaderComponent,
+    UsersManagementComponent,
+    NewUserComponent,
+    LogoutComponent,
+    HasAuthDirective
   ],
   imports: [
     BrowserModule,
@@ -90,14 +119,14 @@ function provideBackendServices(): Provider[] {
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     RouterModule.forRoot(ROUTES_DEFINITIONS),
-    StoreModule.forRoot({authorization: authorityReducer}),
     ModalModule,
+    MomentModule,
+    MatTabsModule,
+    StoreModule.forRoot({authorization: authorityReducer}),
     CommonsModule,
     FormModule,
     DrivermockModule,
-    ParkingMeterModule,
-    AccountMonitoringModule,
-    UsersModule
+    // ParkingMeterModule
   ],
   providers: [
     CookieService,
